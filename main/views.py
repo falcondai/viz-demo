5942bd75cad8dbda44ee401315d1bc1389de5406
Falcon Dai <me@falcondai.com>
2012-12-25 18:51:12 -0600

    graphs completed.

--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from main import app
-from flask import render_template
+from flask import render_template, abort
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 	
-@app.route('/graph')
-def graph():
-	return render_template('graph.html')
+@app.route('/graph/<dataset>')
+def graph(dataset):
+	if dataset == 'wholeyear':
+		return render_template('graph.html', dataset=dataset, freq='daily', order_header='fullperiod', freq_header='counttime', axis_format='%b', tt_format='%x')
+	elif dataset == 'benghazi':
+		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X')
+	elif dataset == '47percent':
+		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X')
+	else:
+		abort(404)
