e3929d109cc4f55f636d9d4e2ae95d4499c98512
Falcon Dai <me@falcondai.com>
2012-12-25 19:14:39 -0600

    index page created.

--- main/views.py
+++ main/views.py
@@ -1,17 +1,21 @@
 from main import app
 from flask import render_template, abort
 
+# graph dimensions
+width = 900
+height = 500
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html', width=width, height=height)
 	
 @app.route('/graph/<dataset>')
 def graph(dataset):
 	if dataset == 'wholeyear':
-		return render_template('graph.html', dataset=dataset, freq='daily', order_header='fullperiod', freq_header='counttime', axis_format='%b', tt_format='%x')
+		return render_template('graph.html', dataset=dataset, freq='daily', order_header='fullperiod', freq_header='counttime', axis_format='%b', tt_format='%x', width=width, height=height)
 	elif dataset == 'benghazi':
-		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X')
+		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X', width=width, height=height)
 	elif dataset == '47percent':
-		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X')
+		return render_template('graph.html', dataset=dataset, freq='hour', order_header='series2', freq_header='Freq', axis_format='%m/%d %H', tt_format='%x %X', width=width, height=height)
 	else:
 		abort(404)
